1ad5f50db11a0a4bfe9655045a7c2e31e6bb5d70
Dampuru Vinod <47512072+damvinod@users.noreply.github.com>
2025-07-16 16:49:29 +0800

    Fix: Changed argument 'acls' to 'acl' in aws_s3_bucket resource

--- s3.tf
+++ s3.tf
@@ -1,4 +1,8 @@
 resource "aws_s3_bucket" "bucket_test" {
   bucket = "test-terraform-bucket-terraform-1234567890"
-  acls   = "private"
+  acl = "private"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_test" {
+  bucket = aws_s3_bucket.bucket_test.bucket
 }
