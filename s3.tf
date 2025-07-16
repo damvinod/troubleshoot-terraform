363510146d402a651960fa1ab75a9738fc61211c
damvinod <vinodreddyreddy25@gmail.com>
2025-07-16 16:48:23 +0800

    Removing terraform format as the fixes may not be aligned for `terraform validate`

--- s3.tf
+++ s3.tf
@@ -2,7 +2,3 @@ resource "aws_s3_bucket" "bucket_test" {
   bucket = "test-terraform-bucket-terraform-1234567890"
   acls   = "private"
 }
-
-resource "aws_s3_bucket_versioning" "bucket_test" {
-  bucket = aws_s3_bucket.bucket_test.bucket
-}
