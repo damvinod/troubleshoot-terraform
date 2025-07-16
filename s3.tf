73760badfa172c74b27c136b42c0a95e20669940
Dampuru Vinod <47512072+damvinod@users.noreply.github.com>
2025-07-16 16:38:50 +0800

    Fix: Replace 'acls' with 'acl' in 'aws_s3_bucket' resource block.

--- s3.tf
+++ s3.tf
@@ -1,8 +1,1 @@
-resource "aws_s3_bucket" "bucket_test" {
-  bucket = "test-terraform-bucket-terraform-1234567890"
-  acls   = "private"
-}
-
-resource "aws_s3_bucket_versioning" "bucket_test" {
-  bucket = aws_s3_bucket.bucket_test.bucket
-}
+{"resource":"aws_s3_bucket","aws_s3_bucket":{"bucket":"test-terraform-bucket-terraform-1234567890","acl":"private"},"resource":"aws_s3_bucket_versioning","aws_s3_bucket_versioning":{"bucket":"aws_s3_bucket.bucket_test.bucket"}}
