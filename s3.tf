4de631b8cf2938c9d4aa976253b506278bbeea23
Dampuru Vinod <47512072+damvinod@users.noreply.github.com>
2025-07-16 16:35:30 +0800

    Corrected the argument name for the access control list of the S3 bucket.

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket_test" {
   bucket = "test-terraform-bucket-terraform-1234567890"
-  acls   = "private"
+  acl = "private"
 }
 
 resource "aws_s3_bucket_versioning" "bucket_test" {
