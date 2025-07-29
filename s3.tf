cbbeb81f3750855c8203e664f9fa71c18e9a0391
Dampuru Vinod <47512072+damvinod@users.noreply.github.com>
2025-07-29 11:49:24 +0800

    Fix: Changed 'acls' to 'acl' for AWS S3 bucket resource in s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,24 @@
+terraform {
+  backend "s3" {
+    region       = "ap-southeast-1"
+    bucket       = "vinod-terraform-test-bucket"
+    key          = "merlion/dev/troubleshoot-terraform"
+    use_lockfile = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_s3_bucket" "bucket_test" {
   bucket = "test-terraform-bucket-terraform-1234567890"
-  acls   = "private"
+  acl = "private"
 }
