cdaf1547174d02c5d1e48444d7b39abd606619c2
Dampuru Vinod <47512072+damvinod@users.noreply.github.com>
2025-07-30 14:09:43 +0800

    Fix: Unsupported argument issue in 's3.tf' file.

--- s3.tf
+++ s3.tf
@@ -1,4 +1,1 @@
-resource "aws_s3_bucket" "bucket_test" {
-  bucket = "test-terraform-bucket-terraform-1234567890"
-  acls = "private"
-}
+{"resource":"aws_s3_bucket","bucket":"test-terraform-bucket-terraform-1234567890","acl":"private"}
